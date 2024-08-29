2b86d4de918f2f808e7fc85c346c3a5a1a562aa5
Guilherme de Oliveira <guilhermeor10@hotmail.com>
2024-08-29 16:11:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [Tarefas, setTarefa] = useState([]);
+  const [newTarefa, setNewTarefa] = useState('');
+
+  const addTarefa = () => {
+    if (newTarefa.trim()) {
+      setTarefa([...Tarefas, { text: newTarefa, completed: false }]);
+      setNewTarefa('');
+    }
+  };
+
+  const toggleCompletion = (index) => {
+    const newTarefa = Tarefas.map((task, i) => {
+      if (i === index) {
+        return { ...task, completed: !Tarefas.completed };
+      }
+      return task;
+    });
+    setTarefa(newTarefa);
+  };
+
+  const removeTask = (index) => {
+    const newTarefa = Tarefas.filter((_, i) => i !== index);
+    setTarefa(newTarefa);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Lista de Tarefas</h1>
+      <p>Aqui você pode adicionar, marcar e remover suas tarefas</p>
+      <input
+        type="text"
+        value={newTarefa}
+        onChange={(e) => setNewTarefa(e.target.value)}
+      />
+      <button onClick={addTarefa}>Adicionar Tarefa</button>
+      <ul>
+        {Tarefas.map((Tarefas, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={Tarefas.completed}
+              onChange={() => toggleCompletion(index)}
+            />
+            {Tarefas.text}
+            <button onClick={() => removeTask(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
